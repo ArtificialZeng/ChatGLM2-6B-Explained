f7cffed1a8403ecb438e3e08dea7a732afaf04ec
曾小健 <2119516028@qq.com>
2023-07-09 12:19:40 +0800

    Update main.py

--- ptuning/main.py
+++ ptuning/main.py
@@ -148,7 +148,7 @@ def main():
 
     # Preprocessing the datasets.
     # We need to tokenize inputs and targets.
-    if training_args.do_train:
+    if training_args.do_train: 
         column_names = raw_datasets["train"].column_names
     elif training_args.do_eval:
         column_names = raw_datasets["validation"].column_names
@@ -159,67 +159,70 @@ def main():
         return
 
     # Get the column names for input/target.
-    prompt_column = data_args.prompt_column
-    response_column = data_args.response_column
-    history_column = data_args.history_column
+    prompt_column = data_args.prompt_column  #从数据参数中获取提示列名称，也就是用于提问的列。
+    response_column = data_args.response_column  #从数据参数中获取回答列的名称，也就是作为回答或目标的列。
+    history_column = data_args.history_column  #从数据参数中获取历史对话列的名称，如果存在的话，这些历史对话将被用作提问的上下文。
     
     # Temporarily set max_target_length for training.
     max_target_length = data_args.max_target_length
 
-    def preprocess_function_eval(examples):
+    #以下是预处理函数，它们用于将输入和目标列进行格式化和分词。格式化的结果将被用于模型的训练和验证。
+    def preprocess_function_eval(examples):  #和preprocess_function_train这两个函数是为评估和训练准备数据的。它们从示例数据中提取问题和回答，并根据需要将其进行格式化和分词。然后它们会将输入和目标添加到model_inputs列表中，然后返回这个列表。
         inputs, targets = [], []
-        for i in range(len(examples[prompt_column])):
-            if examples[prompt_column][i] and examples[response_column][i]:
-                query = examples[prompt_column][i]
-                history = examples[history_column][i] if history_column is not None else None
-                prompt = tokenizer.build_prompt(query, history)
-                inputs.append(prompt)
-                targets.append(examples[response_column][i])
+        for i in range(len(examples[prompt_column])):  #这行代码遍历examples[prompt_column]列表的每一个元素。examples[prompt_column]表示从数据集中提取的问题或提示列。
+            if examples[prompt_column][i] and examples[response_column][i]:  #检查第i个问题/提示和对应的回答是否存在。examples[prompt_column][i]和examples[response_column][i]分别表示第i个问题/提示和对应的回答。如果其中之一不存在，那么就跳过这个样本。
+                query = examples[prompt_column][i]  #将第i个问题/提示赋值给变量query。
+                history = examples[history_column][i] if history_column is not None else None  #检查是否存在历史对话列。如果存在，那么将第i个历史对话赋值给变量history；如果不存在，那么将None赋值给history。
+                prompt = tokenizer.build_prompt(query, history)  #使用分词器的build_prompt函数将问题/提示和历史对话结合起来，生成模型的输入。这通常包括一些特定的格式和分词步骤。
+                inputs.append(prompt)  #将生成的输入添加到inputs列表中。inputs列表将被用作模型的输入。
+                targets.append(examples[response_column][i])  #将第i个回答添加到targets列表中。targets列表将被用作模型的目标。
+                #在这段代码执行之后，你将获得两个列表：inputs和targets。inputs列表包含了所有的输入样本，targets列表包含了所有的目标样本。这两个列表将被用于模型的训练或评估。
 
-        inputs = [prefix + inp for inp in inputs]
-        model_inputs = tokenizer(inputs, max_length=data_args.max_source_length, truncation=True, padding=True)
-        labels = tokenizer(text_target=targets, max_length=max_target_length, truncation=True)
-
-        if data_args.ignore_pad_token_for_loss:
-            labels["input_ids"] = [
+        inputs = [prefix + inp for inp in inputs]  #对于输入列表inputs中的每个元素，都在它们的前面添加一个prefix，然后更新输入列表。这里的prefix可能是一个模型需要的特定前缀，比如特殊的开头标记。
+        model_inputs = tokenizer(inputs, max_length=data_args.max_source_length, truncation=True, padding=True)  #使用tokenizer对更新后的输入进行处理，得到模型的输入。tokenizer是一个将原始文本转换为模型可以理解的形式的工具。这个处理包括截断和填充：如果输入的长度超过了data_args.max_source_length，则会被截断；如果输入的长度小于最大长度，则会被填充到最大长度。得到的model_inputs是一个字典，包含了输入的编码等信息。
+        labels = tokenizer(text_target=targets, max_length=max_target_length, truncation=True)  #对目标（也就是期望的输出）进行同样的处理，得到模型的标签。
+ 
+        if data_args.ignore_pad_token_for_loss:  #如果设置了忽略填充标记的损失，则执行以下步骤：
+            labels["input_ids"] = [  #对于标签中的每个输入ID，如果它是填充标记的ID，则将其替换为-100，否则保持不变。这是因为在计算损失时，我们通常希望忽略填充的部分。在PyTorch中，-100是一个特殊的值，表示在计算损失时忽略这个位置。
                 [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
             ]
-        model_inputs["labels"] = labels["input_ids"]
+        model_inputs["labels"] = labels["input_ids"]  #这行代码将处理后的标签添加到模型的输入中。这样，模型的输入就包含了输入和对应的标签，可以直接用于训练。
 
         return model_inputs
 
+    #函数preprocess_function_train(examples)的主要目标是为模型训练阶段预处理数据。给定一些训练样例examples，它将为每个样例生成模型需要的输入和标签。这个过程包括以下几个步骤：
     def preprocess_function_train(examples):
-        max_seq_length = data_args.max_source_length + data_args.max_target_length + 1
+        max_seq_length = data_args.max_source_length + data_args.max_target_length + 1  #定义最大序列长度为源长度上限（即问题长度上限）加上目标长度上限（即答案长度上限）再加1。这个1通常是为特殊标记（比如序列结束标记）预留的空间。
 
-        model_inputs = {
+        model_inputs = {  #初始化model_inputs字典，用于存储模型输入的数据。
             "input_ids": [],
             "labels": [],
         }
-        for i in range(len(examples[prompt_column])):
-            if examples[prompt_column][i] and examples[response_column][i]:
-                query, answer = examples[prompt_column][i], examples[response_column][i]
+        for i in range(len(examples[prompt_column])):  #遍历每一个样例。
+            if examples[prompt_column][i] and examples[response_column][i]:  #如果样例的问题和答案都存在，那么处理这个样例。
+                query, answer = examples[prompt_column][i], examples[response_column][i]  #获取问题和答案。
 
-                history = examples[history_column][i] if history_column is not None else None
-                prompt = tokenizer.build_prompt(query, history)
+                history = examples[history_column][i] if history_column is not None else None  #获取历史对话，如果存在的话。
+                prompt = tokenizer.build_prompt(query, history)  #用tokenizer.build_prompt方法来根据问题和历史对话构建提示。
 
-                prompt = prefix + prompt
-                a_ids = tokenizer.encode(text=prompt, add_special_tokens=True, truncation=True,
+                prompt = prefix + prompt  #在提示前面添加前缀。
+                a_ids = tokenizer.encode(text=prompt, add_special_tokens=True, truncation=True,  #将提示编码成模型可以理解的形式，得到输入ID序列a_ids。
                                          max_length=data_args.max_source_length)
-                b_ids = tokenizer.encode(text=answer, add_special_tokens=False, truncation=True,
+                b_ids = tokenizer.encode(text=answer, add_special_tokens=False, truncation=True,  #同样地，将答案编码成模型可以理解的形式，得到答案ID序列b_ids。
                                          max_length=data_args.max_target_length)
 
-                context_length = len(a_ids)
-                input_ids = a_ids + b_ids + [tokenizer.eos_token_id]
-                labels = [tokenizer.pad_token_id] * context_length + b_ids + [tokenizer.eos_token_id]
+                context_length = len(a_ids)  #计算输入的长度。
+                input_ids = a_ids + b_ids + [tokenizer.eos_token_id]  #将输入和答案的ID序列拼接起来，并在最后添加一个序列结束标记的ID，得到完整的输入序列。
+                labels = [tokenizer.pad_token_id] * context_length + b_ids + [tokenizer.eos_token_id]  #标签序列的前context_length部分是填充标记的ID，后面是答案的ID序列和一个序列结束标记的ID。这样设置的原因是，我们只关心模型对答案部分的预测。
                 
-                pad_len = max_seq_length - len(input_ids)
-                input_ids = input_ids + [tokenizer.pad_token_id] * pad_len
-                labels = labels + [tokenizer.pad_token_id] * pad_len
-                if data_args.ignore_pad_token_for_loss:
-                    labels = [(l if l != tokenizer.pad_token_id else -100) for l in labels]
+                pad_len = max_seq_length - len(input_ids)  #计算需要填充的长度。
+                input_ids = input_ids + [tokenizer.pad_token_id] * pad_len  #在输入序列后面添加填充标记，使其长度达到max_seq_length。
+                labels = labels + [tokenizer.pad_token_id] * pad_len  #同样地，也在标签序列后面添加填充标记。
+                if data_args.ignore_pad_token_for_loss:  #如果设置了忽略填充标记的损失，那么将标签中的填充标记的ID替换为-100。
+                    labels = [(l if l != tokenizer.pad_token_id else -100) for l in labels]  #将处理好的输入和标签添加到model_inputs字典中。
 
                 model_inputs["input_ids"].append(input_ids)
-                model_inputs["labels"].append(labels)
+                model_inputs["labels"].append(labels)  #在处理完所有样例后，返回model_inputs字典，它包含了所有样例的输入和标签，可以直接用于模型的训练。
 
         return model_inputs
     
